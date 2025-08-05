a8462363cbaabae47b9869898367d4d5e7a833aa
Karthika <66830933+KarthikaGopinath@users.noreply.github.com>
2025-08-05 16:04:42 +0530

    Add files via upload

---
+++ CRUD/src/main/java/com/example/crud/Entity/User.java
@@ -0,0 +1,31 @@
+package com.example.crud.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userDetails")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "username")
+	private String userName;
+
+	@Column(name = "useraddress")
+	private String userAddress;
+
+}
